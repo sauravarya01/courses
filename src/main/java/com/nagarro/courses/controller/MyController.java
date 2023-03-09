778fd2867f204fefd71ec151309e38ccc4e00640
Saurav Arya <saurav.arya01@nagarro.com>
2023-03-09 20:29:05 +0530

    first commit

---
+++ src/main/java/com/nagarro/courses/controller/MyController.java
@@ -0,0 +1,26 @@
+package com.nagarro.courses.controller;
+
+
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class MyController {
+	
+	
+	
+	@GetMapping("/home")
+	public String home() {
+		return "this is home page";
+	}
+	
+
+	
+	
+
+}
+
